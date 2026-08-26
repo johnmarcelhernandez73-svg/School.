1ea7784eeaf088c598377d75e03dfc2761a8b6b2
John_Marcel_Hernandez <johnmarcelhernandez73@gmail.com>
2026-08-26 22:31:17 +0800

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,13 @@
+<?php
+
+    $grade = 90;
+
+    if($grade >= 90){
+        echo "you've Passed this sem";
+    }
+    else{
+        echo "you've failed this sem";
+    }
+
+
+?>
---
+++ ifelseif.php
@@ -0,0 +1,15 @@
+<?php
+
+    $iq = 90;
+
+         if($iq == 100){
+            echo "you are Genius";
+         }
+         elseif($iq >= 80){
+            echo "you are Average";
+         }
+         else{
+            echo "you are below average";
+         }
+
+?>
