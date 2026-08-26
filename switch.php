a4831d7eeb1647342db18ae91cb1604be0369eb8
John_Marcel_Hernandez <johnmarcelhernandez73@gmail.com>
2026-08-26 22:31:44 +0800

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+
+    $power = 3;
+
+         switch($power){
+            case 1:
+                echo "you are a beginner";
+                break;
+            case 2:
+                echo "you are an intermediate";
+                break;
+            case 3:
+                echo "you are an expert";
+                break;
+            default:
+                echo "you are a novice";
+         }
+
+?>
